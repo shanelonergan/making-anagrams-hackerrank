53e3adb62ce5057fa3055eff02be504276c90b22
shanelonergan <sptlonergan@gmail.com>
2020-04-28 12:24:43 -0400

    first soln

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-// Import stylesheets
-import './style.css';
+function makeAnagram(a, b) {
+    const long = a.length >= b.length ? a : b
+    const short = a.length < b.length ? a : b
+    console.log(long, short)
+    let charMapLong = {}
+    let charMapShort = {}
+    let output = 0
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+    for (let char of long.split('').sort()) {
+        charMapLong[char] = charMapLong[char] + 1 || 1
+    }
+
+    for (let char of short.split('').sort()) {
+        charMapShort[char] = charMapShort[char] + 1 || 1
+    }
+
+
+    for (let char in charMapLong) {
+        if (Object.keys(charMapShort).includes(char)) {
+            output += Math.abs(charMapLong[char] - charMapShort[char])
+        } else {
+            output += charMapLong[char]
+        }  
+    }
+
+    for (let char in charMapShort) {
+        if (Object.keys(charMapLong).includes(char)) {
+            output += Math.abs(charMapShort[char] - charMapShort[char])
+        } else {
+            output += charMapShort[char]
+        }  
+    }
+
+    console.log(charMapLong, charMapShort)
+
+
+    console.log('Number of deletions:', output)
+}
+
+makeAnagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke')
